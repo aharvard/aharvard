b4893ee983c9462481c08a9ae76cdf5423b70792
Andrew Harvard <andrew.t.harvard@gmail.com>
2021-09-05 18:33:05 -0400

    update bio

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 #!/usr/bin/env node
-
 var clearConsole = require("clear-any-console");
 
 clearConsole();
 
+const newDate = new Date();
+const yearsWorked = newDate.getFullYear() - 2011;
+
 console.log(`
 Andrew Harvard
 
-Lead UX Engineer with a decade of UI design and development experience, last six years remote. Effective team leader as individual contributor, experienced with defining new working models and optimizing existing processes. Uses code to scale design across organizations with design systems and front-end prototyping. Expertise in crafting inclusive and resilient UIs with semantic HTML and modern CSS. Sharp eye for typography, spacing, and color. 
+Lead UX Engineer with ${yearsWorked} years of UI design and development experience, last six years remote. Effective team leader as individual contributor, experienced with defining new working models and optimizing existing processes. Uses code to scale design across organizations with design systems and front-end prototyping. Expertise in crafting inclusive and resilient UIs with semantic HTML and modern CSS. Sharp eye for typography, spacing, and color. 
 
-Website: https://andrewharvard.com/
-Twitter: https://twitter.com/aharvard
+💻 Website: https://andrewharvard.com/
+🐦 Twitter: https://twitter.com/aharvard
 
 `);
