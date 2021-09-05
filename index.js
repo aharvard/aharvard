b291425616b7f0669aab5027a360b1b8a4300570
Andrew Harvard <andrew.t.harvard@gmail.com>
2021-09-05 18:53:26 -0400

    add welcome

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 #!/usr/bin/env node
-var clearConsole = require("clear-any-console");
-
-clearConsole();
+const pkgJSON = require("./package.json");
+const welcome = require("cli-welcome");
 
 const newDate = new Date();
 const yearsWorked = newDate.getFullYear() - 2011;
 
-console.log(`
-Andrew Harvard
+welcome({
+  title: pkgJSON.name,
+  bgColor: `#FADC00`,
+  color: `#000000`,
+  bold: true,
+  description: pkgJSON.description,
+  version: pkgJSON.version,
+  clear: true,
+});
+
+console.log(`Andrew Harvard
 
 Lead UX Engineer with ${yearsWorked} years of UI design and development experience, last six years remote. Effective team leader as individual contributor, experienced with defining new working models and optimizing existing processes. Uses code to scale design across organizations with design systems and front-end prototyping. Expertise in crafting inclusive and resilient UIs with semantic HTML and modern CSS. Sharp eye for typography, spacing, and color. 
 
