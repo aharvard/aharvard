f435034a3738d58fc0da65efda11a81a6c1917dd
Andrew Harvard <andrew.t.harvard@gmail.com>
2021-09-05 15:08:32 -0400

    update bio

--- index.js
+++ index.js
@@ -5,5 +5,11 @@ var clearConsole = require("clear-any-console");
 clearConsole();
 
 console.log(`
-Andrew Harvard - UX Engineer
+Andrew Harvard
+
+Senior UX Engineer at Regions Bank. Currently, developing UIs for the bank's websites, consulting on front-end systems architecture, and a core member of of Balance, an enterprise-wide design system.
+
+Website: https://andrewharvard.com/
+Twitter: https://twitter.com/aharvard
+
 `);
